02f33450b0a7b95a9496d7689d45e5b27c841920
Tilak Paneru <blak.ros3s@gmail.com>
2025-05-17 16:53:36 +0545

    Update login page UI

---
+++ frontend/src/pages/Add.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const Add = () => {
+  return (
+    <div>
+      This is the add page
+    </div>
+  );
+}
+
+export default Add;
